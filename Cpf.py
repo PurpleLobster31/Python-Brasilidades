65169abffdb40891ce093cddc0aff2f8df8db8a7
PurpleLobster31 <lopesmatheus642@gmail.com>
2022-08-24 18:02:55 -0300

    Fim aula 1

---
+++ Cpf.py
@@ -0,0 +1,24 @@
+from validate_docbr import CPF
+
+class Cpf:
+    def __init__(self, documento):
+        documento = str(documento)
+        if self.valida_cpf(documento):
+            self.cpf = documento
+        else:
+            raise ValueError("CPF Inválido!")
+    
+    def __str__(self):
+        return self.formata_cpf()
+    
+    def valida_cpf(self, documento):
+        if len(documento) == 11:
+            validador = CPF()
+            return validador.validate(documento)
+        else:
+            raise ValueError("Quantidade de dígitos inválida!")
+        
+    def formata_cpf(self):
+        mascara = CPF()
+        return mascara.mask(self.cpf)
+        
---
+++ main.py
@@ -0,0 +1,4 @@
+from Cpf import Cpf
+
+cpf_um = Cpf("41042164886")
+print(cpf_um)
